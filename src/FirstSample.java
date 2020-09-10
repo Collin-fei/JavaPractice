ab67bf3f2c1f5d4b712756f801c83c6ba6bbe858
Collinfei30 <861622558@qq.com>
2020-09-10 11:13:25 +0800

    FirstSample

---
+++ src/FirstSample.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Administrator on 2020/9/10.
+ */
+public class FirstSample {
+    public static void main(String[] args) {
+        System.out.println("we will not use Hello World");
+    }
+}
